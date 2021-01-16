131736a4b4bb80e1e7f1a1c53aa60d41c39debc2
Abdullah Oğuz <oguzabdullah39@gmail.com>
2021-01-16 23:20:37 +0300

    initial commit,

---
+++ ArtBookProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBookProject
+//
+//  Created by Abdullah Oğuz on 15.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
